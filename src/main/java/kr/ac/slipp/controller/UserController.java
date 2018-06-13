d2ba523ffe56312656f6e43ee78f7d6b9a6bdefe
alforik <neo0603@naver.com>
2018-06-13 10:28:22 +0900

    현재버전 커밋.. 커밋까지 안했었군..

--- src/main/java/kr/ac/slipp/controller/UserController.java
+++ src/main/java/kr/ac/slipp/controller/UserController.java
@@ -1,18 +1,17 @@
 package kr.ac.slipp.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.slipp.domain.User;
 import kr.ac.slipp.dto.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	//private List<User> users=new ArrayList<User>();  // 이게 여기 있는게 맞나? 더 전역에 있어야 할필요는.. new 로 생성해야하나? autowire는
@@ -20,18 +19,25 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		//users.add(user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
 		
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/users/list";
 	}
+	
+	@GetMapping("/form")
+	public String form() {
+		
+		return "/users/form";
+	}
+	
 
 }
